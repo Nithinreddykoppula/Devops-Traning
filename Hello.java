a76eec5d22234e5bf9eb8ef06fa77d29b29e574a
nithin <koppulanithinreddy@gmail.com>
2024-07-26 16:17:48 -0400

    Add read me file

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-System.out.println ("welcome to gits online traning!"); 
+System.out.println ("welcome to gits online traning!!!!!"); 
 
     // Write your code here
     }
