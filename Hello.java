b1e02386688b2f3d6752c67af577b90fa911b0b1
nithin <koppulanithinreddy@gmail.com>
2024-07-26 16:34:05 -0400

    Revert "Updating the file"
    This reverts commit a09217335f6cbf6541ee891a01eba1e7a219996a.

--- Hello.java
+++ Hello.java
@@ -2,9 +2,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 
-
-
-System.out.println ("welcome to Devops online  traning!!!!!"); 
+System.out.println ("welcome to gits online traning!!!!!"); 
 
     // Write your code here
     }
