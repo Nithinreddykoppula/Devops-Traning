c289fba4d9f83ac3f31a2c11ec4437b7d74e9ec8
nithin <koppulanithinreddy@gmail.com>
2024-07-26 15:14:28 -0400

    My First commint of Java file

---
+++ Hello.java
@@ -0,0 +1,16 @@
+//
+class HelloWorld {
+    public static void main(String[] args) {
+
+System.out.println ("welcome to gits online traning!"); 
+
+    // Write your code here
+    }
+}
+
+
+
+
+
+
+
